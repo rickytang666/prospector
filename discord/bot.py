5fafb6c7007f24d1454683ff02242cbc319ee9a8
juliansalvador727 <juliansalvador727@gmail.com>
2026-03-07 13:31:59 -0500

    config bot

--- discord/bot.py
+++ discord/bot.py
@@ -1,14 +1,26 @@
-import logging
-import json
-import os
-import platform
-import random
-import sys
-
+import asyncio
 import discord
+from discord.ext import commands
+from config import TOKEN
 
-from discord.ext import commands, tasks
-from discord.ext.commands import Context
-from dotenv import load_dotenv
+intents = discord.Intents.default()
 
-load_dotenv()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+bot.team_configs = {}
+bot.team_context_cache = {}
+
+async def load_cogs():
+    await bot.load_extension("cogs.setup_team")
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+    await bot.tree.sync()
+
+async def main():
+    async with bot:
+        await load_cogs()
+        await bot.start(TOKEN)
+
+asyncio.run(main())
